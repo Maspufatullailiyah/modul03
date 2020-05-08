6337973026caee5f5d8b2ad2d47d97e34feddd77
Maspufatullailiyah <maspufa@gmail.com>
2020-05-08 02:47:40 -0700

    Menambahkan listener

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -1,6 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiByText extends JFrame {
+
+    JLabel labell = new JLabel("Coba Input 1");
+    JTextField textField1 = new JTextField();
+    JButton button1 = new JButton("Greet");
+
     GuiByText(){
         setSize(640,480);
         setLocationRelativeTo(null);
@@ -8,8 +15,30 @@ public class GuiByText extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    void  meletakkanKomponen(){
+        getContentPane().setLayout(null);
+
+        getContentPane().add(labell);
+        getContentPane().add(textField1);
+
+        labell.setBounds(16, 32, 100, 25);
+        textField1.setBounds(124, 32, 100, 25);
+        button1.setBounds(232, 32, 80, 25);
+    }
+
+    void menambahkanListener(){
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String nama = textField1.getText();
+                JOptionPane.showMessageDialog(null,"Hai" + nama);
+            }
+        });
+    }
+
     public static void main(String[] args) {
         GuiByText guiByText = new GuiByText();
+        guiByText.meletakkanKomponen();
         guiByText.setVisible(true);
     }
 }
